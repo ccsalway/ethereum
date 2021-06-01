965681b3ebb77eeae45e400c88741db6ebfcc065
Christian Salway <ccsalway@icloud.com>
2021-06-01 16:11:29 +0100

    bingo

---
+++ bingo.py
@@ -0,0 +1,82 @@
+import random
+
+# there is an edge case where a card isnt filled out and hits the iterative limit
+while 1:
+
+    seqs = [
+        list(range(1, 10 + 1)),
+        list(range(11, 20 + 1)),
+        list(range(21, 30 + 1)),
+        list(range(31, 40 + 1)),
+        list(range(41, 50 + 1)),
+        list(range(51, 60 + 1)),
+        list(range(61, 70 + 1)),
+        list(range(71, 80 + 1)),
+        list(range(81, 90 + 1))
+    ]
+
+    cards = [[{'col': c, 'vals': []} for c in range(9)] for _ in range(6)]
+
+    # add at least one number to each column
+    for card in cards:
+        for c in range(len(card)):
+            # randomly select a number
+            n = random.choice(seqs[c])
+            # remove that number from further selection
+            seqs[c].remove(n)
+            # place the number in the relevant column
+            card[c]['vals'].append(n)
+
+    # distribute remaining numbers across cards
+    limit = 0
+    while limit < 1000:
+        # ensure no card has more than 15 numbers
+        unfilled = [card for card in cards if sum([len(col['vals']) for col in card]) < 15]
+        for card in unfilled:
+            # filter out sequences with no values
+            avail = [(idx, vals) for idx, vals in enumerate(seqs) if len(vals) > 0]
+            if not avail: break
+            # select a random column to assign a number
+            idx, vals = random.choice(avail)
+            # ensure column doesnt already have 3 numbers
+            if len(card[idx]['vals']) == 3:
+                continue
+            # randomly select a number
+            n = random.choice(vals)
+            # remove the chosen number from further selection
+            seqs[idx].remove(n)
+            # place the number in the relevant column
+            card[idx]['vals'].append(n)
+        # end loop if no more numbers available
+        if not unfilled: break
+        if not avail: break
+        limit += 1
+
+    if limit < 1000:
+        break
+
+# organise columns into rows
+for card in cards:
+
+    rows = [[' ' for _ in range(9)] for _ in range(3)]
+
+    # sort seqs into rows of no more than 5 numbers
+    for s in [3, 2, 1]:
+        fcols = list(filter(lambda n: len(n['vals']) == s, card))
+        for col in fcols:
+            # loop over each value and assign to a row
+            r = random.randrange(3)
+            for i in range(s):
+                # ensure we add to a row with space
+                while rows[r].count(' ') <= 4 or rows[r][col['col']] != ' ':
+                    r += 1
+                    if r > 2:
+                        r = 0
+                # add value to row,column
+                rows[r][col['col']] = col['vals'][i]
+
+    # print rows
+    for row in rows:
+        print(row)
+    
+    print(' ' * 20)
--- contracts/bingo.sol
+++ contracts/bingo.sol
@@ -1,27 +1,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+/*
+UK 90-ball bingo (9x3 tickets)
+numbers are arranged in columns 1-9, 10-19, 20-29, ..., 80-89
+each row contains five numbers and four blank spaces
+each column contains up to three numbers
+each ticket has 15 numbers
+each page has 6 tickets
+Rewards:
+15% 1 line
+25% 2 lines
+40% 3 lines
+20% host
+
+due to the compute used to reset the mapping of players,
+launch a new contract for each game
+*/
+
 contract Bingo {
 
-    mapping(address => bool) admins;
-    mapping(address => uint[]) players;
+    address private host;
+    uint256 private ticketPrice;  // wei
+    mapping(address => string) private players;  // string holds ticket numbers
 
-    constructor() {
-        admins[msg.sender] = true;
+    constructor(uint256 _ticketPrice) {
+        host = msg.sender;
+        ticketPrice = _ticketPrice;
     }
 
-    function enableAdmin(address admin) public {
-        require(admins[msg.sender] == true);
-        admins[admin] = true;
-    }
-
-    function disableAdmin(address admin) public {
-        require(admins[msg.sender] == true);
-        admins[admin] = false;
-    }
-
-    function isAdmin(address admin) public view returns (bool) {
-        return admins[admin];
+    function getTicketPrice() public view returns (uint256) {
+        return ticketPrice;
     }
 
 }
