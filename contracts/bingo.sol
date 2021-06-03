c7f6391ef8c8400e7310c77b23daf1545ebdf0c6
Christian Salway <ccsalway@icloud.com>
2021-06-03 09:46:23 +0100

    bingo was his namo

--- contracts/bingo.sol
+++ contracts/bingo.sol
@@ -1,36 +1,25 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-/*
-UK 90-ball bingo (9x3 tickets)
-numbers are arranged in columns 1-9, 10-19, 20-29, ..., 80-89
-each row contains five numbers and four blank spaces
-each column contains up to three numbers
-each ticket has 15 numbers
-each page has 6 tickets
-Rewards:
-15% 1 line
-25% 2 lines
-40% 3 lines
-20% host
-
-due to the compute used to reset the mapping of players,
-launch a new contract for each game
-*/
+pragma solidity ^0.8.4;
 
 contract Bingo {
 
-    address private host;
-    uint256 private ticketPrice;  // wei
-    mapping(address => string) private players;  // string holds ticket numbers
+    address public host;
+    uint256 public ticketPrice;
+    mapping(address => int8[][]) public players;
 
-    constructor(uint256 _ticketPrice) {
+    error InsufficientPayment(uint256 ticketPrice, uint256 paid);
+    event Purchased(address player,  int8[][] numbers);
+
+    constructor(uint256 _ticketPriceWei) {
         host = msg.sender;
-        ticketPrice = _ticketPrice;
+        ticketPrice = _ticketPriceWei;
     }
 
-    function getTicketPrice() public view returns (uint256) {
-        return ticketPrice;
+    function buyTicket(int8[][] memory numbers) public payable {
+        if (msg.value < ticketPrice)
+            revert InsufficientPayment({ticketPrice: ticketPrice, paid: msg.value});
+        players[msg.sender] = numbers;
+        emit Purchased(msg.sender, numbers);
     }
 
 }
--- src/bingo/bingo5.py
+++ src/bingo/bingo5.py
@@ -31,12 +31,14 @@ def generate_ticket():
         ttlnums = tkt[0][c] + tkt[1][c] + tkt[2][c]
         rndnums = sorted([get_random_number(c) for _ in range(ttlnums)])
         for r in [2, 1, 0]:
-            tkt[r][c] = ' ' if tkt[r][c] == 0 else rndnums.pop()
+            if tkt[r][c] == 1:
+                tkt[r][c] = rndnums.pop()
     # return
     return tkt
 
 
 if __name__ == '__main__':
     tkt = generate_ticket()
+    print(tkt)
     for row in tkt:
         print(row)
