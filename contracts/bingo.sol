523406140e29e57a520d3e499efa3f71a4d9f3ba
Christian Salway <ccsalway@icloud.com>
2021-06-03 21:30:15 +0100

    bingo contract

--- contracts/bingo.sol
+++ contracts/bingo.sol
@@ -1,25 +1,63 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPL-3.0
+
 pragma solidity ^0.8.4;
 
 contract Bingo {
-
     address public host;
+    bool public enabled;
     uint256 public ticketPrice;
-    mapping(address => int8[][]) public players;
 
-    error InsufficientPayment(uint256 ticketPrice, uint256 paid);
-    event Purchased(address player,  int8[][] numbers);
+    struct Ticket {
+        uint8[27] numbers; // ticket is 3 rows by 9 numbers = 27 numbers
+    }
 
-    constructor(uint256 _ticketPriceWei) {
+    mapping(address => Ticket[]) players;
+
+    event NewPlayer(address player, uint8[27] ticket);
+
+    constructor(uint256 ticketPriceWei) {
         host = msg.sender;
-        ticketPrice = _ticketPriceWei;
+        enabled = true;
+        ticketPrice = ticketPriceWei;
     }
 
-    function buyTicket(int8[][] memory numbers) public payable {
-        if (msg.value < ticketPrice)
-            revert InsufficientPayment({ticketPrice: ticketPrice, paid: msg.value});
-        players[msg.sender] = numbers;
-        emit Purchased(msg.sender, numbers);
+    function switchOnOff() public {
+        require(msg.sender == host, "Caller is not host");
+        enabled = enabled ? false : true;
     }
 
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function getTickets(address player) public view returns (Ticket[] memory) {
+        return players[player];
+    }
+
+    function buyTicket(uint8[27] memory ticket) payable public {
+        require(enabled, "Game is disabled");
+        require(msg.value >= ticketPrice, "Insufficient payment");
+
+        players[msg.sender].push(Ticket(ticket));
+
+        // refund overpayment
+        uint256 refund = msg.value - ticketPrice;
+        (bool success, ) = msg.sender.call{value: refund}("");
+        require(success, "Refund failed");
+       
+        // host can watch for this event to be informed of new players
+        emit NewPlayer(msg.sender, ticket);
+    }
+
+    function payout(address player, uint256 amountWei) public {
+        require(msg.sender == host, "Caller is not host");
+        (bool success, ) = player.call{value: amountWei}("");
+        require(success, "Payout failed");
+    }
+
+    function withdrawBalance() public {
+        require(msg.sender == host, "Caller is not host");
+        (bool success, ) = host.call{value: address(this).balance}("");
+        require(success, "Withdraw failed");
+    }
 }
